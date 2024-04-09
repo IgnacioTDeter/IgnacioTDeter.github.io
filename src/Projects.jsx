75d03412b81ab8a79170b201e4c7e49573b30916
IgnacioTDeter <detergithub@gmail.com>
2024-04-09 14:18:58 -0300

    Agregamos Lenguajes a los proyectos

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -6,13 +6,13 @@ export function Projects() {
         {
             id: 1,
             text: "Wordle es un juego simple, donde los jugadores intentan adivinar una palabra de cinco letras en seis intentos.",
-            languages: ["JavaScript", "HTML", "CSS"],
+            languages: [<HTML />, <CSS />, <JavaScript/>, <React/>],
             title: "Wordle"
         },
         {
             id: 2,
-            text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis, alias velit. Consequuntur blanditiis id ea hic vel? Expedita officiis quam",
-            languages: ["JavaScript", "HTML", "CSS"],
+            text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis, alias velit. Consequuntur blanditiis id ea hic vel? Expedita officiis quamLorem ipsum dolor sit amet consectetur adipisicing elit. Soluta nam, corrupti repellat quas aut deserunt culpa consectetur tempora quidem reprehenderit nisi harum natus aspernatur molestiae? Velit natus ipsa incidunt repellat?",
+            languages: [<HTML />, <CSS />, <JavaScript/>, <React/>],
             title: "Sistema de gestion 'Pañol'"
         }
     ]
@@ -30,7 +30,7 @@ export function Projects() {
                     <article>
                         <h1>{project.title}</h1>
                         {project.text ? project.text : null}
-                        <div>
+                        <div className="dis-flex">
                             {project.languages ? project.languages : null}
                         </div>
                     </article>
@@ -38,5 +38,52 @@ export function Projects() {
 
             ))}
         </section>
+    )
+}
+
+export function HTML() {
+    return (
+        <div className="language-container">
+            HTML
+            <div>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/800px-HTML5_logo_and_wordmark.svg.png" alt="" />
+            </div>
+        </div>
+    )
+
+}
+
+export function CSS() {
+    return (
+        <div className="language-container">
+            CSS
+            <div>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export function JavaScript() {
+
+    return (
+        <div className="language-container">
+            JavaScript
+            <div>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png" alt="" />
+            </div>
+        </div>
+    )
+
+}
+
+export function React(){
+    return (
+        <div className="language-container">
+            React
+            <div>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png" alt="" />
+            </div>
+        </div>
     )
 }
